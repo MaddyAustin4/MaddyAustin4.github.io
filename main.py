19065a81bbb7abe1c937549371b78cb2e4e19efa
MaddyAustin4 <madeleine.austin@sainsburys.co.uk>
2021-03-10 14:30:58 +0000

    emr deletion test bulk

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 from pdf2image import convert_from_path
 from PIL import Image
-# images_from_path = convert_from_path('EDpatientinfoleaflet.pdf', output_folder='output.txt')
+import os
 
-im = Image.open("31e06b2b-2fa4-47f3-988d-ae21201b289b-1.ppm")
-im.save("sweet_pic.jpg")
+def pdf_to_jpg():
+    convert_from_path('EDpatientinfoleaflet.pdf', output_folder='.', output_file='output')
+    names = []
+    for image in os.listdir('.'):
+        if image.startswith('output'):
+            im = Image.open(image)
+            image_name = f"{image}.jpg"
+            names.append(image_name)
+            im.save(image_name)
+    return names
+
+def to_index():
+    with open("index.html", "a") as myfile:
+        for i in pdf_to_jpg():
+            str = f"<a href=EDpatientinfoleaflet.pdf class=image fit><img src={i} alt=></a>\n"
+            myfile.write(str)
+
+
+if __name__ == "__main__":
+    to_index()
