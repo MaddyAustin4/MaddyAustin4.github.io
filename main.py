dd8409dacb91325f6b27d97121ea894099c41f24
MaddyAustin4 <madeleine.austin@sainsburys.co.uk>
2021-03-10 14:44:24 +0000

    automated

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from PIL import Image
 import pyqrcode
 import os
 
+
 def pdf_to_jpg():
     convert_from_path('EDpatientinfoleaflet.pdf', output_folder='.', output_file='output')
     names = []
@@ -14,12 +15,14 @@ def pdf_to_jpg():
             im.save(image_name)
     return names
 
+
 def to_index():
     os.system('rm -f index.html')
     with open("index.html", "a") as myfile:
         for i in pdf_to_jpg():
-            str = f"<a href=EDpatientinfoleaflet.pdf class=image ><img src={i} alt=></a>\n"
+            str = f'<img src="{i}" width="145" height="126" alt="Planets"usemap="#planetmap">\n'
             myfile.write(str)
+
 
 def create_qr_code():
     url = pyqrcode.create('https://maddyaustin4.github.io/')
