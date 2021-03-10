35a570f158bf2b7939780698030737ca0527b18c
MaddyAustin4 <madeleine.austin@sainsburys.co.uk>
2021-03-10 14:45:33 +0000

    automated

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ def create_qr_code():
 
 
 if __name__ == "__main__":
+    os.remove('new.png')
     for image in os.listdir('.'):
         if image.startswith('output'):
             os.remove(image)
